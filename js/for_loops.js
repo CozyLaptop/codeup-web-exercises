d67f6214c69a9c2b7ac305b02b62b0aa4381cfc3
Albert Dominguez <ajd@Alberts-MacBook-Air.local>
2021-07-15 22:01:31 -0500

    finished for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,33 +1,34 @@
-function showMultiplicationTable(number){
-    for(var i = 1; i <= 10; i++) {
+function showMultiplicationTable(number) {
+    for (var i = 1; i <= 10; i++) {
         console.log(number + " x " + i + " = " + number * i);
     }
 }
+
 showMultiplicationTable(7);
 //////////////////////////////
 
 //generates a random number between and including min and max
 function generateRandomInteger(min, max) {
-    return Math.floor(min + Math.random()*(max + 1 - min))
+    return Math.floor(min + Math.random() * (max + 1 - min))
 }
 
-function randomNumbers20200(){
-    for(let i = 0; i <= 10; i++){
+function randomNumbers20200() {
+    for (let i = 0; i <= 10; i++) {
         let randomNumber = generateRandomInteger(20, 200);
-        if(randomNumber % 2 === 0) {
+        if (randomNumber % 2 === 0) {
             console.log(randomNumber + " is " + "even");
-        }
-        else{
+        } else {
             console.log(randomNumber + " is " + "odd");
         }
     }
 }
+
 randomNumbers20200();
 
 ///////////////////////////
-function numberPyramid(){
+function numberPyramid() {
     let string = "";
-    for(let i = 1; i <= 9; i++) {
+    for (let i = 1; i <= 9; i++) {
         for (let j = 1; j <= i; j++) {
             string = string + i.toString();
         }
@@ -35,12 +36,15 @@ function numberPyramid(){
         string = "";
     }
 }
+
 numberPyramid();
+
 //////////////////////////
-function countdownByFive(){
-    for (let i = 100; i > 0; i - 5){
+function countdownByFive() {
+    for (let i = 100; i > 0; i - 5) {
         console.log(i);
         i -= 5;
     }
 }
+
 countdownByFive();
