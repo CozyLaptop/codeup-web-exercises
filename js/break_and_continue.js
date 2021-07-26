64f6a87f8bd549b1e8abf412bc18e073e724691d
Albert Dominguez <albert.j.dominguez21@gmail.com>
2021-07-26 15:32:13 -0500

    added presentations for css

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,11 +3,12 @@ var numberToSkip = 0;
 while(numberToSkip % 2 === 0) {
     numberToSkip = prompt("Enter an odd number");
 }
-console.log("Number to skip")
-for (var i = 0; i > 0; i++){
-    if (i === numberToSkip){
+console.log("Number to skip is: " + numberToSkip);
+for (var i = 1; i < 50; i+= 2){
+    if (parseFloat(numberToSkip) === i){
         console.log("Yikes! Skipping number " + numberToSkip);
-        continue;
     }
-    console.log("Here is an odd number: " + i);
+    else {
+        console.log("Here is an odd number: " + i);
+    }
 }
