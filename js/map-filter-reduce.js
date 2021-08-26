04ca4dd74a8cdecdc52e76c55d21a760acbcdc0c
Albert Dominguez <albert.j.dominguez21@gmail.com>
2021-08-26 14:30:56 -0500

    initial commit and finished .filter exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,47 @@
+var testText = document.getElementById("test");
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var newUsers = users.filter(function(user) {
+    return user.languages.length > 2;
+});
+console.log(newUsers);
+
+testText.innerHTML = newUsers;
