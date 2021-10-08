299ad24925b8a65ba679986fb389cda6a5e53da6
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-10-08 09:03:13 -0500

    finished promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,6 @@
-fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
+const url = 'https://api.github.com/users/CozyLaptop/events';
+let user = "CozyLaptop"
+fetch(url, {headers: {'Authorization': GITHUB_API_TOKEN}})
+    .then(response => response.json()).then(response => {
+        console.log(response[0])
+})
