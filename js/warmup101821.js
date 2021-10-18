febb3d7c1bcef53cc1da3c5d2b92b27bda894c7f
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-10-18 09:40:50 -0500

    exercise v2

--- js/warmup101821.js
+++ js/warmup101821.js
@@ -1,3 +1,9 @@
-$("#hide-btn").click(()=> {
-    $("#paragraph").toggle();
+// $("#hide-btn").click(()=> {
+//     $("#paragraph").toggle();
+// })
+var button = document.getElementById("hide-btn");
+var paragraph = document.getElementById("paragraph");
+var words = paragraph.innerText;
+button.addEventListener("click", ()=>{
+    paragraph.style.display = "none";
 });
